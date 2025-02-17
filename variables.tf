2748771c2d7a2bfc0ea07f6216bcd955cc4f8063
Siarhei_Kirakasian <Siarhei.Kirakasian@ivanti.com>
2025-02-17 22:43:07 +0400

    add-content

--- variables.tf
+++ variables.tf
@@ -1,13 +1,11 @@
 variable "domain_name" {
   description = "The domain name for the static site."
   type        = string
-  default     = ${{ secrets.DOMAIN_NAME }}
 }
 
 variable "certificate_arn" {
   description = "The ARN of the existing ACM certificate."
   type        = string
-  default     = "your_arn"
 }
 
 variable "content_directory" {
